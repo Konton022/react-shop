538908c7ab47be756da33879187a9c61f75680d0
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-12-24 19:22:04 +0500

    add fetch effect

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import { Cards } from './Cards';
 
 const Shop = () => {
-    return <main className='container content'></main>;
+    const [items, setItems] = useState([]);
+    console.log(API_KEY, API_URL);
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            headers: { Authorization: API_KEY },
+        };
+        fetch(API_URL, requestOptions)
+            .then((resp) => resp.json())
+            .then((data) => {
+                setItems(data.shop);
+                console.log(data.shop);
+            });
+    }, []);
+    return (
+        <main className='container content'>
+            {items.map((item) => (
+                <Cards
+                    key={item.mainId}
+                    title={item.displayName}
+                    url={item.displayAssets[0].full_background}
+                />
+            ))}
+        </main>
+    );
 };
 
 export { Shop };
--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL =
-    'https://fortniteapi.io/v2/items/get?id=CID_242_Athena_Commando_F_Bullseye&lang=ru';
+const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru';
 
 export { API_KEY, API_URL };
